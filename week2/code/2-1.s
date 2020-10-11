c2c466f46f2cce4e8fbae1711c25cd83a0f89893
panda <2414616363@qq.com>
2020-10-11 20:14:52 +0800

    chmod文件权限的测试

---
+++ week2/code/2-1.s
@@ -0,0 +1,38 @@
+	.file	"2-1.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%d=hello!\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	cmpl	$99, -4(%rbp)
+	jg	.L2
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	jmp	.L3
+.L2:
+	movl	$0, %eax
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
