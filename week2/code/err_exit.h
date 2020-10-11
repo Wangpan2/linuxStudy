b9545bd454b1b46a7adfd11e3862b449fa709cac
panda <2414616363@qq.com>
2020-10-11 23:04:19 +0800

    课本上P21页的例子1-2

---
+++ week2/code/err_exit.h
@@ -0,0 +1,9 @@
+#include <errno.h>
+#include <stdlib.h> 
+#define err_exit(MESSAGE) (                \
+    perror(MESSAGE),                       \
+    exit(1)                                \
+)
+
+
+
